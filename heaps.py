5c475e1a71c422d91018a954821516a6acf22154
Yvonne Yeh <code@yvonneyeh.com>
2021-06-10 17:38:25 -0700

    Initial commit

---
+++ heaps.py
@@ -0,0 +1,28 @@
+from Heap import Heap
+
+class PriorityQueue: # Comments indicate what methods the Heap class must have
+    def __init__(self):
+        self.queue_heap = Heap()
+
+    def insert(self, item):
+        """Put a new item in the queue"""
+        self.queue_heap.insert(item) # insert
+
+    def get_max(self):
+        """Return the max item in the queue"""
+        return self.queue_heap.get_max() # get_max
+
+    def remove_max(self):
+        """Return and remove the max item from the queue"""
+        return self.queue_heap.remove_max() # remove_max
+
+    def get_size(self):
+        """Return the size"""
+        return self.queue_heap.get_size() # get_size
+
+    def is_empty(self):
+        """Return true if the queue is empty"""
+        return self.queue_heap.is_empty() # is_empty
+
+    def __str__(self):
+        return self.queue_heap.__str__() # __str__
